01f51c6bfdf65dd552b0df15bdaefae3f73e3480
WeatheredComb21 <mateusz.bala02@gmail.com>
2021-02-19 18:12:06 +0100

    first commit

---
+++ src/Frame.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+    private static final long serialVersionUID = 1L;
+    public Frame(){
+        super("Kółko i krzyżyk");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300,400);
+        setLocation(700,100);
+        setVisible(true);
+
+    
+    }
+
+
+}
