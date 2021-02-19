fd207800945939411e98741416be7505f2e2d573
WeatheredComb21 <mateusz.bala02@gmail.com>
2021-02-19 19:39:04 +0100

    test

--- src/Frame.java
+++ src/Frame.java
@@ -1,4 +1,5 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Frame extends JFrame{
     private static final long serialVersionUID = 1L;
@@ -7,7 +8,12 @@ public class Frame extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300,400);
         setLocation(700,100);
+        JPanel buttonsPanel = new Buttons();
+        add(buttonsPanel);
+        
         setVisible(true);
+
+    
 
     
     }
