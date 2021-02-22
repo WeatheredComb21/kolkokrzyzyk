8799a3f1c1d3ae6c84422e56e704844680124a09
WeatheredComb21 <mateusz.bala02@gmail.com>
2021-02-22 16:32:10 +0100

    wa

--- src/Buttons.java
+++ src/Buttons.java
@@ -5,31 +5,49 @@ import java.awt.event.ActionListener;
 
 public class Buttons extends JPanel implements ActionListener {
     private static final long serialVersionUID = 1L;
+    boolean myTurn = true;
     JButton reset;
     JButton[] button = new JButton[9];
 
     public Buttons() {
 
         for (int i = 0; i < button.length; i++) {
-            button[i] = new JButton(""+i);
+            button[i] = new JButton("");
             button[i].addActionListener(this);
             add(button[i]);
 
         }
         reset = new JButton("RESET");
+        reset.addActionListener(this);
         add(reset);
 
         setLayout(new GridLayout(4, 3));
     }
 
     public void actionPerformed(ActionEvent e) {
-        
+
         Object source = e.getSource();
+
+        if(source .equals(reset)){
+            for (int i = 0; i < button.length; i++){
+                button[i].setText("");
+                button[i].setEnabled(true);
+            }
+        }
+
+        myTurn = !myTurn;
+
+        String symbol = "X";
+        if(myTurn){
+            symbol = "O";
+        }
+
 
         for (int i = 0; i < button.length; i++){
 
             if(source.equals(button[i])){
-                button[i].setText("X");
+                button[i].setText(symbol);
+                button[i].setEnabled(false);
             }
         }
 
